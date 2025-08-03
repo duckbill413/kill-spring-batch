8bf7a8ef63fd00a5fbb7a6df40f08e41110fd317
duckbill413 <uhyeon7399@daum.net>
2025-08-03 22:16:00 +0900

    System.exit, SpringApplication.exit 으로 시작하여 exit code를 외부 시스템에 노출

--- strategy-spring-batch/src/main/java/com/system/batch/KillBatchSystemApplication.java
+++ strategy-spring-batch/src/main/java/com/system/batch/KillBatchSystemApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KillBatchSystemApplication {
 
+	/**
+	 * 배치 시스템에서는 SpringApplication.run() 의 결과를 System.exit() 로 처리하는 것이 권장됨
+	 * 배치 작업의 성공/실패 상태를 exit code 로 외부 시스템에 전달할 수 있기 때문에 배치 모니터링과 제어에 필수적
+	 */
 	public static void main(String[] args) {
-		SpringApplication.run(KillBatchSystemApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(KillBatchSystemApplication.class, args)));
 	}
 
 }
